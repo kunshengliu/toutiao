adc6a6258f0a6bb4f7a3a6a4a6bc06cd96d1bb29
liukunsheng <liukunsheng@Matrimax-PC>
2017-05-31 21:30:33 +0800

    update

---
+++ src/main/java/com/uestc/model/EntityType.java
@@ -0,0 +1,6 @@
+package com.uestc.model;
+
+public class EntityType {
+	public static int ENTITY_NEWS = 1;//表示新闻资讯
+	
+}
---
+++ src/main/resources/toutiao.sql
@@ -0,0 +1,142 @@
+-- MySQL dump 10.13  Distrib 5.6.19, for Win64 (x86_64)
+--
+-- Host: localhost    Database: toutiao
+-- ------------------------------------------------------
+-- Server version	5.6.23-enterprise-commercial-advanced-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `comment`
+--
+
+DROP TABLE IF EXISTS `comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `comment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `content` text NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `entity_id` int(11) NOT NULL,
+  `entity_type` int(11) NOT NULL,
+  `created_date` datetime NOT NULL,
+  `status` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `entity_index` (`entity_id`,`entity_type`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comment`
+--
+
+LOCK TABLES `comment` WRITE;
+/*!40000 ALTER TABLE `comment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `login_ticket`
+--
+
+DROP TABLE IF EXISTS `login_ticket`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `login_ticket` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `ticket` varchar(45) NOT NULL,
+  `expired` datetime NOT NULL,
+  `status` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ticket_UNIQUE` (`ticket`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `login_ticket`
+--
+
+LOCK TABLES `login_ticket` WRITE;
+/*!40000 ALTER TABLE `login_ticket` DISABLE KEYS */;
+INSERT INTO `login_ticket` VALUES (4,9,'312923ae-9151-4d32-812e-7b961f0ee59f','2017-05-13 11:04:39',1),(5,9,'eb0bd1e8-4762-4bfe-a4d6-936bb8aa20ff','2017-05-13 11:14:33',0),(6,9,'9f160f50-27e3-462a-a8e9-60d6895428c0','2017-05-14 16:55:04',1),(7,10,'bfb12ec3-52f3-4a94-8a16-0334fb933596','2017-05-14 16:57:01',1),(8,11,'cff96803-afb1-40b3-9262-59d3c867203f','2017-05-16 22:02:17',0),(9,9,'5471ac21-ea98-4f47-b051-c94a9e652714','2017-05-27 21:01:40',0);
+/*!40000 ALTER TABLE `login_ticket` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `news`
+--
+
+DROP TABLE IF EXISTS `news`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `news` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(128) NOT NULL DEFAULT '',
+  `link` varchar(256) NOT NULL DEFAULT '',
+  `image` varchar(256) NOT NULL DEFAULT '',
+  `like_count` int(11) NOT NULL,
+  `comment_count` int(11) NOT NULL,
+  `created_date` datetime NOT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `news`
+--
+
+LOCK TABLES `news` WRITE;
+/*!40000 ALTER TABLE `news` DISABLE KEYS */;
+INSERT INTO `news` VALUES (1,'阿里巴巴','http://www.alibaba.com','http://oqjtgt6eg.bkt.clouddn.com/da71bceb-cf03-4501-9298-61d2e90ac44f.jpg',0,0,'2017-05-26 22:11:35',9);
+/*!40000 ALTER TABLE `news` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL DEFAULT '',
+  `password` varchar(128) NOT NULL DEFAULT '',
+  `salt` varchar(32) NOT NULL DEFAULT '',
+  `head_url` varchar(256) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (9,'1165777973@qq.com','6E17FD8EFAF940623F276CBCB2D15DFE','9e6c6','http://images.nowcode.com/head/187t.png'),(10,'x','0CEA9553719E4F6B992A47D2AD4DCFC5','ffb81','http://images.nowcode.com/head/276t.png'),(11,'xs','6EDF594198AE90C3BCCFF6C4564787A1','eee4f','http://images.nowcode.com/head/475t.png');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-05-31 21:29:55
