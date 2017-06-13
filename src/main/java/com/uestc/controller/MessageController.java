134bab03c02d5a5ccaf522da4d3e4fe202b3dea6
liukunsheng <liukunsheng@Matrimax-PC>
2017-06-13 22:56:42 +0800

    UPDATE

---
+++ src/main/java/com/uestc/controller/MessageController.java
@@ -0,0 +1,36 @@
+package com.uestc.controller;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.uestc.model.Message;
+import com.uestc.service.MessageService;
+import com.uestc.util.ToutiaoUtils;
+
+@Controller
+public class MessageController {
+	@Autowired
+	private MessageService messageService;
+	
+	@RequestMapping("/msg/addMessage")
+	@ResponseBody
+	public String addMessage(@RequestParam("fromId") int fromId,
+			@RequestParam("toId") int toId,
+			@RequestParam("content") String content
+			){
+		Message msg = new Message();
+		msg.setContent(content);
+		msg.setCreatedDate(new Date());
+		msg.setFromId(fromId);
+		msg.setToId(toId);
+		msg.setConversationId(fromId < toId ? String.format("%d_%d",fromId,toId):String.format("%d_%d", toId, fromId));
+		//存入
+            return ToutiaoUtils.getJSONString(msg.getId());
+		
+	}
+}
